e422dfe4223119470a3aef9961ad41f7398ab872
beingsamvis@gmail.com <beingsamvis@gmail.com>
2023-08-02 11:11:54 +0530

    HookJs

--- src/App.js
+++ src/App.js
@@ -1,37 +1,103 @@
-import {Component} from  'react';
-class Counter extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0
+import {useState, useEffect} from 'react';
+
+const MyInfo = () => {
+  const [data, setData] = useState([]);
+
+  useEffect (() => {
+    const fetching = async () => {
+      try{
+        const res = await fetch('https://catfact.ninja/fact');
+        const js = await res.json();
+        setData(js.fact);
+      }
+      catch(error){
+        console.log("Error");
+      }
+    }
+    fetching();
+
+    return() => {
     };
-  }
-      
-  increment = () => {
-    this.setState((prev) => ({
-      count: prev.count + 1
-    }));
-  };
-  
-  decrement = () => {
-    this.setState((prev) => ({
-      count: prev.count-1
-    }));
-  };
 
+  }, []);
 
-      
-  render() {
-    return (
-      <div className='counter'>
-        <h2>Counter: {this.state.count}</h2>
-        <div className='buttons'>
-          <button onClick={this.increment}>+</button>
-          <button onClick={this.decrement}>-</button>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>MyInformation</h1>
+      <p>{data}</p>
+    </div>
+
+  );
 }
-      
-export default Counter;
+
+export default MyInfo;
+
+
+
+
+
+
+
+// import { useState } from "react";
+
+// function Hooks(){
+//     const [count, setCount] = useState(0);
+//     const counting = () => {
+//         setCount(count+1);
+//     };
+
+//     return(
+//         <div>
+//             <h2>Count: {count}</h2>
+//             <button onClick={counting}></button>
+//         </div>
+//     );
+
+
+// }
+
+// export default Hooks;
+
+
+// import {Component} from  'react';
+// class Counter extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       count: 0
+//     };
+//   }
+
+//   increment = () => {
+//     this.setState((i) => ({
+//       count: i.count + 1
+//     }));
+//   };
+
+//   decrement = () => {
+//     this.setState((i) => ({
+//       count: i.count - 1
+//     }));
+//   };
+
+//   makeItZero = () => {
+//     this.setState(() => ({
+//       count: 0
+//     }));
+//   };
+
+//   render() {
+//     return (
+//       <div className='counter'>
+//         <h2>Counter: {this.state.count}</h2>
+//         <div className='buttons'>
+//           <button onClick={this.increment}>+</button>
+//           <button onClick={this.decrement}>-</button>
+//           <button onClick={this.makeItZero}>Reset</button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
